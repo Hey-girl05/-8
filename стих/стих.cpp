af618596e39c899cbbb162e25f7951036e797f09
Лиза <lizamitina05@mail.ru>
2024-01-16 21:17:54 +0300

    Добавьте файлы проекта.

---
+++ стих/стих.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+
+namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	std::cout << "Что ж ты, мишка-шалунишка,";
+	std::cout << "Перепачкал все штанишки?";
+	std::cout << "За обедом баловался";
+	std::cout << "И кефиром обливался."
+
+}
+
+
+
